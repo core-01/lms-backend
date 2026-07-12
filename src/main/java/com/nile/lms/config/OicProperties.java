8bef909a4b41aef8c69341e49697c5429109f339
Raghu <core.raghvendra@gmail.com>
2026-07-13 01:07:29 +0530

    Fix: register OicProperties as a Spring bean

--- src/main/java/com/nile/lms/config/OicProperties.java
+++ src/main/java/com/nile/lms/config/OicProperties.java
@@ -1,7 +1,9 @@
 package com.nile.lms.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties(prefix = "oic")
 public class OicProperties {
     private String registerUrl;
